a746155f3fc7de95b3952fd91cc44a951b8f13bc
Slaier <30682486+Slaier@users.noreply.github.com>
2022-06-05 07:04:15 +0000

    feat: make nix fmt available

--- flake.nix
+++ flake.nix
@@ -17,5 +17,8 @@
       packages = forAllSystems (system: import ./default.nix {
         pkgs = import nixpkgs { inherit system; };
       });
+      formatter = forAllSystems (system:
+        (let pkgs = import nixpkgs { inherit system; }; in pkgs.nixpkgs-fmt)
+      );
     };
 }
